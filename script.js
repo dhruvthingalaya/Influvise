e17dbe60d16c7a7c6a5e40d64ca583f0d29f3e8d
Dhruv Thingalaya <dhruvthingalaya.contact@gmail.com>
2025-03-08 04:44:39 +0530

    images and About

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.querySelector('#whoClientsDialog')
+const Clients = document.getElementById("Clients");
+const clientsDialog = document.getElementById("clientsDialog");
+const closeClients = document.getElementById("closeClients");
+
+Clients.addEventListener("click", () => { clientsDialog.showModal() });
+closeClients.addEventListener("click", () => { closeClients.close() });
