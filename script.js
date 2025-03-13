b2f6bc9a800f8f6c319bfc55c86a6cba03560d68
Dhruv Thingalaya <dhruvthingalaya.contact@gmail.com>
2025-03-13 16:56:48 +0530

    contact js update

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // Application form validation
-    const form = document.querySelector(".form");
+    const form = document.querySelector(".user-forms");
     if (form) {
         form.setAttribute("novalidate", true);
         const inputs = form.querySelectorAll("input[required], textarea[required]");
